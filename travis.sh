712b4c5f8e3b8d5da7cf2f6d1f86029c074a2139
Nikita Gryzlov <nixel2007@gmail.com>
2018-01-16 12:15:00 +0300

    Подключение SQ

---
+++ travis.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+#opm run coverage
+
+temp=`cat src/cmd/Модули/ПараметрыПриложения.os | grep "Версия = " | sed 's|[^"]*"||' | sed -r 's/".+//'`
+version=${temp##*|}
+
+if [ "$TRAVIS_SECURE_ENV_VARS" == "true" ]; then
+  if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+    sonar-scanner \
+        -Dsonar.host.url=http://sonar.silverbulleters.org \
+        -Dsonar.analysis.mode=issues \
+        -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+        -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+        -Dsonar.github.oauth=$SONAR_GITHUB_TOKEN \
+        -Dsonar.login=$SONAR_TOKEN \
+        -Dsonar.scanner.skip=false
+
+  elif [ "$TRAVIS_BRANCH" == "develop" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    sonar-scanner \
+        -Dsonar.host.url=http://sonar.silverbulleters.org \
+        -Dsonar.login=$SONAR_TOKEN \
+        -Dsonar.projectVersion=$version\
+        -Dsonar.scanner.skip=false
+  fi
+fi
