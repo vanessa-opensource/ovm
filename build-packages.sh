d5ed29ec58f8fda2f4d31415dc3f0ae6e3eef216
Alexey Lustin <alustin@silverbulleters.org>
2018-06-17 13:55:35 +0300

    работающий сборщик deb пакетов

--- build-packages.sh
+++ build-packages.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-docker-compose build deb-builder
-docker-compose up deb-builder
+
+opm install -l
+oscript -make src/cmd/ovm.os ovm.exe
+mv ovm.exe builders/deb/
+VERSION=`mono ./builders/deb/ovm.exe -v`
+
+docker-compose -f builders/docker-compose.yml build deb-builder
+docker-compose -f builders/docker-compose.yml up deb-builder
 
 #docker-compose up 
 #TODO - надо подумать на тестовым запуском - фактически мы должны проверить что пакеты установились и можно установить движок и какую-нибудь библиотеку: например logos
--- builders/deb/build.sh
+++ builders/deb/build.sh
@@ -4,4 +4,31 @@ echo "Starting build deb"
 
 source /etc/profile
 
-dpkg-buildpackage -us -uc
+DEBBUILDROOT=/opt/deb/
+
+if [ -d "$DEBBUILDROOT" ]; then
+    rm -rf $DEBBUILDROOT
+    mkdir -p $DEBBUILDROOT
+fi
+
+PAKNAME=onescript-version-manager
+DSTPATH=${DEBBUILDROOT}${PAKNAME}
+
+mkdir -p $DSTPATH
+mkdir -p $DSTPATH/DEBIAN
+mkdir -p $DSTPATH/usr/bin
+mkdir -p $DSTPATH/usr/share/ovm/bin
+mkdir -p $DSTPATH/etc/bash_completion.d
+
+cp /opt/debian/* $DSTPATH/DEBIAN/
+cp /opt/builder/ovm $DSTPATH/usr/bin
+cp /opt/builder/ovm.exe $DSTPATH/usr/share/ovm/bin
+cp /opt/builder/ovm-completion $DSTPATH/etc/bash_completion.d
+
+fakeroot dpkg-deb --build $DSTPATH
+
+rm -rf $DSTPATH
+chmod 777 $DSTPATH.deb
+dpkg-name -o $DSTPATH.deb
+
+ls -al /opt/deb
